c48fe8c18b506239ba809ec81f2b2d062f41162d
ayokunle22 <122690826+ayokunle22@users.noreply.github.com>
2023-02-22 19:28:03 -0600

    added ip.sh file

---
+++ ip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#show me my IP Adress
+
+echo " My IP is : $(ip a | grep inet | grep enp0s3 | awk '{print $2}' | awk -F/ '{print $1}') "
+echo " My username is: $(whoami) "
+echo " My ssh is: $(systemctl status sshd | grep Active) "
